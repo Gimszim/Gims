f8bdb6a5f320a9ddbaa45b90ec66039bbf37b90b
Gimszim <77892613+Gimszim@users.noreply.github.com>
2021-01-23 23:54:42 +0200

    Initialize optimize-sof

---
+++ config/EURUSD_M15.h
@@ -0,0 +1,31 @@
+/*
+ * @file
+ * Defines default strategy parameter values for the given timeframe.
+ */
+
+// Defines indicator's parameter values for the given pair symbol and timeframe.
+struct Indi_RSI_Params_M15 : Indi_RSI_Params {
+  Indi_RSI_Params_M15() : Indi_RSI_Params(indi_rsi_defaults, PERIOD_M15) {
+    applied_price = (ENUM_APPLIED_PRICE)2;
+    period = 12;
+    shift = 0;
+  }
+} indi_rsi_m15;
+
+// Defines strategy's parameter values for the given pair symbol and timeframe.
+struct Stg_RSI_Params_M15 : StgParams {
+  // Struct constructor.
+  Stg_RSI_Params_M15() : StgParams(stg_rsi_defaults) {
+    lot_size = 0;
+    signal_open_method = 0;
+    signal_open_filter = 36;
+    signal_open_level = 30;
+    signal_open_boost = 0;
+    signal_close_method = -4;
+    signal_close_level = 10;
+    price_stop_method = 1;
+    price_stop_level = 20;
+    tick_filter_method = 1;
+    max_spread = 0;
+  }
+} stg_rsi_m15;
